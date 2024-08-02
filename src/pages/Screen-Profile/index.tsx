86750790eed1781c63a4236ec400f3ee3a046a07
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-02 19:35:49 -0300

    add: Creating screen of profile

---
+++ src/pages/Screen-Profile/index.tsx
@@ -0,0 +1,34 @@
+//Importações
+import { ScrollView, View, Text } from "react-native";
+
+//Componentes
+import { HeaderApresentation } from "../../components/Header-Apresentation"
+
+//Estilização
+import { ContainerProfile } from "./style"
+import { DetailsProfile } from "../../components/Details-Profile";
+import { PresentMap } from "../../components/Present-Map";
+import { VaccinationRequest } from "../../components/Vaccination-Request";
+
+export const ScreenProfile = () => {
+    return (
+        <ScrollView>
+            <ContainerProfile>
+                <HeaderApresentation
+                    title={"Perfil"}
+                    placeholder="Pesquisar perfil"
+                />
+
+                <DetailsProfile
+                    userEmail="userAsclepius@gmail.com"
+                    userName="User Test"
+                    userPhone="(00) 0 0000-0000"
+                />
+
+                <PresentMap />
+
+                <VaccinationRequest/>
+            </ContainerProfile>
+        </ScrollView>
+    )
+}
