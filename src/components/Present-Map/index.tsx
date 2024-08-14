4b5be9776eb0892ca7f2418bd42fe3ddb6284507
José Gabriel <jgabrielfd0@gmail.com>
2024-08-14 15:01:33 -0300

    refacto: Adding new attributes to Presen-Map

--- src/components/Present-Map/index.tsx
+++ src/components/Present-Map/index.tsx
@@ -1,73 +1,100 @@
-//Importações 
-import React, {useState, useEffect} from "react";
+//Importações
+import React, { useState, useEffect } from "react";
 import { View, Alert, StyleProp, ViewStyle } from "react-native";
-import MapView, {Marker} from "react-native-maps";
-import * as Location from 'expo-location';
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 //Estilizações
 import { styles } from "./style";
 
 //props
 interface props {
-    latitude?: string,
-    longitude?: string,
-    pattent?: keyof typeof styles, //Se tem a possibilidade de reutiliza esse código estabelecendo uma nova estilização com novos valores
+  latitude?: string;
+  longitude?: string;
+  zoomEnable?: boolean;
+  scrollEnable?: boolean;
+  showLocationButtom?: boolean;
+  pattent?: keyof typeof styles; //Se tem a possibilidade de reutiliza esse código estabelecendo uma nova estilização com novos valores
 }
 
-export const PresentMap:React.FC<props> = ({latitude, longitude, pattent = "container"}) => {
-    const mapStyle = styles[pattent] as StyleProp<ViewStyle>; //Estilização 
-    
-    const [currentLocation, setCurrentLocation] = useState<{ latitude: number; longitude: number } | null>(null);
+export const PresentMap: React.FC<props> = ({
+  latitude,
+  longitude,
+  zoomEnable = true,
+  scrollEnable = true,
+  showLocationButtom = true,
+  pattent = "container",
+}) => {
+  const mapStyle = styles[pattent] as StyleProp<ViewStyle>; //Estilização
 
-    // Seria interresante adicionar uma função para listar no mapa do usuário
-    // uma lista de eventos próximos a sua localização
+  const [currentLocation, setCurrentLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
 
-    //O ideal é que isso já seja pego ao adentrar o sistema, somente exemplo
-    //Depois refatorar
-    useEffect(() => {
-        (async () => {
-            if (!latitude || !longitude) {
-                // Solicitar permissão de localização
-                let { status } = await Location.requestForegroundPermissionsAsync();
-                if (status !== 'granted') {
-                    Alert.alert('Permissão negada', 'A permissão de localização é necessária para mostrar o mapa.');
-                    return;
-                }
+  // Seria interresante adicionar uma função para listar no mapa do usuário
+  // uma lista de eventos próximos a sua localização
 
-                // Obter a localização atual
-                let location = await Location.getCurrentPositionAsync({});
-                setCurrentLocation({
-                    latitude: location.coords.latitude,
-                    longitude: location.coords.longitude,
-                });
-            }
-        })();
-    }, [latitude, longitude]);
+  //O ideal é que isso já seja pego ao adentrar o sistema, somente exemplo
+  //Depois refatorar
+  useEffect(() => {
+    (async () => {
+      if (!latitude || !longitude) {
+        // Solicitar permissão de localização
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          Alert.alert(
+            "Permissão negada",
+            "A permissão de localização é necessária para mostrar o mapa."
+          );
+          return;
+        }
 
-    const mapRegion = {
-        latitude: latitude ? parseFloat(latitude) : currentLocation?.latitude || 37.78825,
-        longitude: longitude ? parseFloat(longitude) : currentLocation?.longitude || -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    };
-    
-    return(
-        <View style={mapStyle}>
-            <MapView 
-                style={styles.containerMapProfile}
-                region={mapRegion}
-                showsUserLocation={true}
-            >
-                {(latitude && longitude) || currentLocation ? (
-                    <Marker
-                        coordinate={{
-                            latitude: latitude ? parseFloat(latitude) : currentLocation?.latitude || 0,
-                            longitude: longitude ? parseFloat(longitude) : currentLocation?.longitude || 0,
-                        }}
-                        title="Minha Localização"
-                    />
-                ) : null}
-            </MapView>
-        </View>
-    )
-}
+        // Obter a localização atual
+        let location = await Location.getCurrentPositionAsync({});
+        setCurrentLocation({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        });
+      }
+    })();
+  }, [latitude, longitude]);
+
+  const mapRegion = {
+    latitude: latitude
+      ? parseFloat(latitude)
+      : currentLocation?.latitude || 37.78825,
+    longitude: longitude
+      ? parseFloat(longitude)
+      : currentLocation?.longitude || -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  return (
+    <View style={mapStyle}>
+      <MapView
+        style={styles.containerMapProfile}
+        region={mapRegion}
+        showsUserLocation={true}
+        zoomEnabled={zoomEnable}
+        scrollEnabled={scrollEnable}
+        showsMyLocationButton={showLocationButtom}
+      >
+        {(latitude && longitude) || currentLocation ? (
+          <Marker
+            coordinate={{
+              latitude: latitude
+                ? parseFloat(latitude)
+                : currentLocation?.latitude || 0,
+              longitude: longitude
+                ? parseFloat(longitude)
+                : currentLocation?.longitude || 0,
+            }}
+            title="Minha Localização"
+          />
+        ) : null}
+      </MapView>
+    </View>
+  );
+};
--- src/components/Present-Map/style.ts
+++ src/components/Present-Map/style.ts
@@ -3,20 +3,27 @@ import { StyleSheet } from "react-native";
 import { Themes } from "../../../global/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        width: '95%',
-        height: 520,
-        borderRadius: 20,
-    },
+  container: {
+    width: "95%",
+    height: 520,
+    borderRadius: 20,
+  },
 
-    containerMapProfile: {
-        width: '100%',
-        height: '100%',
-        borderRadius: 20,
-    },
+  containerMapProfile: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 20,
+  },
 
-    containerMediumPattent: {
-        width: '100%',
-        height: '100%',
-    }
-})
+  mapEvent: {
+    width: 128,
+    height: 128,
+    borderRadius: 20,
+    overflow: "hidden",
+  },
+
+  containerMediumPattent: {
+    width: "100%",
+    height: "100%",
+  },
+});
