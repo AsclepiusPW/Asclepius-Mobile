573c75a8b2e6b4cb76d5139c21d291e6b068a1d2
José Gabriel <jgabrielfd0@gmail.com>
2024-08-18 20:01:18 -0300

    add: Creating a test array

---
+++ utils/tests/arrayInfoVaccine.ts
@@ -0,0 +1,29 @@
+import { infoVaccine } from "../types/typeInfoVaccine";
+
+export const arrayInfoVaccines: infoVaccine[] = [
+  {
+    manufacturer: "Pfizer",
+    type: "mRNA",
+    title: "Comirnaty",
+  },
+  {
+    manufacturer: "Moderna",
+    type: "mRNA",
+    title: "Spikevax",
+  },
+  {
+    manufacturer: "AstraZeneca",
+    type: "Viral Vector",
+    title: "Vaxzevria",
+  },
+  {
+    manufacturer: "Johnson & Johnson",
+    type: "Viral Vector",
+    title: "Janssen",
+  },
+  {
+    manufacturer: "Sinovac",
+    type: "Inactivated Virus",
+    title: "CoronaVac",
+  },
+];
