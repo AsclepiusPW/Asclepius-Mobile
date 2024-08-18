a26d4a36aaaa1cb487ff84b91e4139ac84bd7f29
José Gabriel <jgabrielfd0@gmail.com>
2024-08-18 20:05:54 -0300

    add: Creating the Screen-List-Vaccine

---
+++ src/pages/Screen-List-Vaccine/index.tsx
@@ -0,0 +1,79 @@
+import { ActivityIndicator, ScrollView, View } from "react-native";
+import { HeaderApresentation } from "../../components/Header-Apresentation";
+import React from "react";
+import styles from "./style";
+import VaccineRegister from "../../components/Vaccine-Register";
+import { useEffect, useState } from "react";
+import { infoVaccine } from "../../../utils/types/typeInfoVaccine";
+import { arrayInfoVaccines } from "../../../utils/tests/arrayInfoVaccine";
+import { Themes } from "../../../global/theme";
+import { NoRecordView } from "../../components/No-Record-View";
+
+const ScreenListVaccine = () => {
+  const [infoVaccines, setInfoVaccines] =
+    useState<infoVaccine[]>(arrayInfoVaccines);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [noRecords, setNoRecords] = useState<boolean>(false);
+
+  const searchInfoVaccines = (query: string) => {
+    setSearchQuery(query.toLowerCase());
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    setNoRecords(false);
+
+    setTimeout(() => {
+      if (searchQuery === "") {
+        setInfoVaccines(arrayInfoVaccines);
+        setNoRecords(false);
+      } else {
+        const searchInfoVaccines = arrayInfoVaccines.filter((info) =>
+          info.title.toLowerCase().includes(searchQuery)
+        );
+
+        setInfoVaccines(searchInfoVaccines);
+        setNoRecords(searchInfoVaccines.length === 0);
+      }
+      setLoading(false);
+    }, 1000);
+  }, [searchQuery]);
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.headerApresentation}>
+          <HeaderApresentation
+            search={true}
+            title="Vacinas Registradas"
+            placeholder="Pesquisar vacinas..."
+            changeSubmit={searchInfoVaccines}
+          />
+        </View>
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color={`${Themes.colors.greenDark}`}
+          />
+        ) : (
+          <View style={styles.viewList}>
+            {noRecords ? (
+              <NoRecordView title="Nenhum Evento Encontrado" />
+            ) : (
+              infoVaccines.map((content, index) => (
+                <VaccineRegister
+                  key={index}
+                  manufacturer={content.manufacturer}
+                  title={content.title}
+                  type={content.type}
+                />
+              ))
+            )}
+          </View>
+        )}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ScreenListVaccine;
---
+++ src/pages/Screen-List-Vaccine/style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  headerApresentation: { marginBottom: 10 },
+  viewList: {
+    width: "100%",
+    height: "auto",
+    alignItems: "center",
+  },
+});
+
+export default styles;
