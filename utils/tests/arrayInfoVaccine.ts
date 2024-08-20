27807426b1969f47ebc0739fc4bbe95afece2b97
José Gabriel <jgabrielfd0@gmail.com>
2024-08-20 19:48:29 -0300

    feat: Adding new attributes to arrayInfoVaccine

--- utils/tests/arrayInfoVaccine.ts
+++ utils/tests/arrayInfoVaccine.ts
@@ -2,28 +2,48 @@ import { infoVaccine } from "../types/typeInfoVaccine";
 
 export const arrayInfoVaccines: infoVaccine[] = [
   {
-    manufacturer: "Pfizer",
-    type: "mRNA",
-    title: "Comirnaty",
+    description:
+      "A vacina contra a COVID-19 ajuda a prevenir infecções graves e mortes causadas pelo coronavírus.",
+    contraindications:
+      "Indivíduos com alergia grave a qualquer componente da vacina ou que tiveram uma reação alérgica grave a uma dose anterior não devem receber a vacina.",
+    manufacturer: "Pfizer-BioNTech",
+    type: "Vacina contra COVID-19",
+    title: "Covid-19",
   },
   {
-    manufacturer: "Moderna",
-    type: "mRNA",
-    title: "Spikevax",
+    description:
+      "A vacina contra influenza (gripe) é utilizada para prevenir infecções por vírus influenza, que podem causar doenças respiratórias graves.",
+    contraindications:
+      "Indivíduos com alergia grave a ovos, ou que tiveram uma reação alérgica grave a uma dose anterior da vacina, devem evitar a vacinação.",
+    manufacturer: "GSK (GlaxoSmithKline)",
+    type: "Vacina contra influenza",
+    title: "Influenza",
   },
   {
-    manufacturer: "AstraZeneca",
-    type: "Viral Vector",
-    title: "Vaxzevria",
+    description:
+      "A vacina contra o vírus da hepatite B é altamente eficaz na prevenção da infecção pelo vírus da hepatite B (HBV).",
+    contraindications:
+      "Pessoas com alergia grave à levedura de padeiro ou a qualquer outro componente da vacina não devem ser vacinadas.",
+    manufacturer: "Sanofi Pasteur",
+    type: "Vacina contra hepatite B",
+    title: "Hepatite B",
   },
   {
-    manufacturer: "Johnson & Johnson",
-    type: "Viral Vector",
-    title: "Janssen",
+    description:
+      "A vacina contra a febre amarela é usada para prevenir esta infecção viral, que pode ser fatal.",
+    contraindications:
+      "Indivíduos com alergia grave a ovos ou imunossuprimidos não devem receber esta vacina.",
+    manufacturer: "Bio-Manguinhos/Fiocruz",
+    type: "Vacina contra febre amarela",
+    title: "Febre Amarela",
   },
   {
-    manufacturer: "Sinovac",
-    type: "Inactivated Virus",
-    title: "CoronaVac",
+    description:
+      "A vacina contra o tétano é usada para prevenir infecções causadas pela bactéria Clostridium tetani.",
+    contraindications:
+      "Indivíduos com alergia grave a qualquer componente da vacina não devem ser vacinados.",
+    manufacturer: "Sanofi Pasteur",
+    type: "Vacina contra tétano",
+    title: "Tétano",
   },
 ];
