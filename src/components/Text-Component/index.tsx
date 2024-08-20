78d4af0c8efe66615401d10c067b59a9a2099598
José Gabriel <jgabrielfd0@gmail.com>
2024-08-20 19:39:31 -0300

    feat: Creating custom styles in Text-Componen

--- src/components/Text-Component/index.tsx
+++ src/components/Text-Component/index.tsx
@@ -1,20 +1,49 @@
-import { Text, View } from "react-native";
+import { Text, TextStyle, View, ViewStyle } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import styles from "./style";
+import React from "react";
 
 type props = {
   iconName?: string;
   text?: string;
   subTitle?: string;
   title?: string;
+  textStyles?: TextStyle;
 };
-const TextComponent = ({ iconName, text, subTitle, title }: props) => {
+
+const TextComponent = ({
+  iconName,
+  text,
+  subTitle,
+  title,
+  textStyles,
+}: props) => {
   return (
     <View style={styles.container}>
-      {title ? <Text style={styles.title}>{title}</Text> : ""}
-      {iconName ? <Icon name={iconName} style={styles.styledIcon} /> : ""}
-      {subTitle ? <Text style={styles.subTitle}>{subTitle + ": "}</Text> : ""}
-      {text ? <Text>{text}</Text> : ""}
+      {title ? (
+        <Text style={[styles.title]} numberOfLines={2} ellipsizeMode="tail">
+          {title}
+        </Text>
+      ) : (
+        ""
+      )}
+      {iconName ? (
+        <Icon style={[styles.styledIcon, textStyles]} name={iconName} />
+      ) : (
+        ""
+      )}
+      {subTitle ? (
+        <Text style={[styles.subTitle, textStyles]}>{subTitle + ": "}</Text>
+      ) : (
+        ""
+      )}
+      {text ? (
+        <Text style={[textStyles]} numberOfLines={1} ellipsizeMode="tail">
+          {text}
+        </Text>
+      ) : (
+        ""
+      )}
     </View>
   );
 };
