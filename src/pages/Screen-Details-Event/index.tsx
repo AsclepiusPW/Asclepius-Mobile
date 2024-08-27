2a47a01bb34ec49cb010a3efac28f0a434b544ba
José Gabriel <jgabrielfd0@gmail.com>
2024-08-27 19:44:15 -0300

    add: Creating  the Screen-Details-Event

---
+++ src/pages/Screen-Details-Event/index.tsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, ScrollView, View } from "react-native";
+
+import PresentDetails from "../../components/Present-Details";
+import styles from "./style";
+import { Themes } from "../../../global/theme";
+import { NoRecordView } from "../../components/No-Record-View";
+import { arrayEvent } from "../../../utils/tests/arrayEvent";
+import { Event } from "../../../utils/types/typeEvent";
+import { arrayInfoVaccines } from "../../../utils/tests/arrayInfoVaccine";
+import { PresentMap } from "../../components/Present-Map";
+import { infoVaccine } from "../../../utils/types/typeInfoVaccine";
+import EventComponent from "../../components/Event-Component";
+import { TouchButton } from "../../components/Touch-Button";
+
+type Prop = {
+  localName: string;
+};
+
+const ScreenDetailsEvent = ({ localName }: Prop) => {
+  const formatDate = (dateString: string): string => {
+    const [year, month, day] = dateString.split("-");
+    return `${day}/${month}/${year}`;
+  };
+
+  const [event, setEvent] = useState<Event>();
+  const [infoVaccine, setInfoVaccine] = useState<infoVaccine>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [noRecords, setNoRecords] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setNoRecords(false);
+
+    setTimeout(() => {
+      const selectedEvent = arrayEvent.find(
+        (event) => event.localName === localName
+      );
+      setEvent(selectedEvent);
+
+      const selectedVaccine = arrayInfoVaccines.find(
+        (event) => event.title === selectedEvent?.vacineName
+      );
+      setInfoVaccine(selectedVaccine);
+
+      if (!selectedVaccine && !selectedEvent) {
+        setNoRecords(true);
+      }
+      setLoading(false);
+    }, 1000);
+  }, [localName]);
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color={`${Themes.colors.greenDark}`}
+          />
+        ) : event && infoVaccine ? (
+          <View style={styles.viewList}>
+            <View style={styles.mapContainer}>
+              <PresentMap
+                pattent="mapEventDetails"
+                latitude={`${event.latitude}`}
+                longitude={`${event.longitude}`}
+              />
+            </View>
+            <PresentDetails
+              details={[
+                {
+                  iconName: "syringe",
+                  label: "Vacina",
+                  text: event.vacineName,
+                },
+                {
+                  iconName: "calendar",
+                  label: "Data",
+                  text: formatDate(event.dateEvent),
+                },
+                {
+                  iconName: "calendar-minus",
+                  label: "Status",
+                  text: "Não Realizado",
+                },
+              ]}
+              title={event.localName}
+            />
+            <PresentDetails
+              title={infoVaccine.title}
+              contraindications={infoVaccine.contraindications}
+              description={infoVaccine.description}
+              details={[
+                {
+                  iconName: "truck",
+                  label: "Fabricante",
+                  text: infoVaccine.manufacturer,
+                },
+                {
+                  iconName: "sliders-h",
+                  label: "Tipo",
+                  text: infoVaccine.type,
+                },
+              ]}
+            />
+            <PresentDetails
+              title="Mais Informações"
+              details={[
+                {
+                  iconName: "user-alt",
+                  label: "Responsável",
+                  text: event.localName,
+                },
+              ]}
+              observation={infoVaccine.observations}
+            />
+            <View style={styles.buttonContainer}>
+              <TouchButton
+                text="Solicitar Reserva de Vaga"
+                styleType="buttonLargerSolid"
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchButton
+                text="Ver Outros Eventos"
+                styleType="buttonLargerOpacity"
+              />
+            </View>
+          </View>
+        ) : (
+          <NoRecordView title="Vacina não encontrada" />
+        )}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ScreenDetailsEvent;
---
+++ src/pages/Screen-Details-Event/style.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  mapContainer: {
+    alignItems: "center",
+    width: "100%",
+    height: 256,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    marginBottom: 10,
+  },
+  viewList: {
+    width: "100%",
+    height: "auto",
+    alignItems: "center",
+  },
+});
+
+export default styles;
