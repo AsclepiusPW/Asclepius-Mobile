06018156b4740e77e3dd704948c391900b13cd0d
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-30 14:00:15 -0300

    feat: Creating global navigation typing

---
+++ @types/navigation.d.ts
@@ -0,0 +1,21 @@
+//Depois atualizar os valores das rotas
+
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Initial: undefined;
+      Login: undefined;
+      SignUp: undefined;
+      TabNaviagtion: undefined; // Para a navegação das abas
+      Home: undefined;
+      ListVaccines: undefined;
+      ListEvents: undefined;
+      VaccinationRegisters: undefined;
+      Profile: undefined;
+      DetailsEvent: undefined;
+      DetailsVaccine: undefined;
+      EditProfile: undefined;
+      VaccinationRequest: undefined;
+    }
+  }
+}
