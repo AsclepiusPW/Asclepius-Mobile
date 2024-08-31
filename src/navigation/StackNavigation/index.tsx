fc77e398fe05a9c2a00e222394cf7a9356b499f5
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-31 17:47:27 -0300

    feat: Authenticating navigation based on authentication context

--- src/navigation/StackNavigation/index.tsx
+++ src/navigation/StackNavigation/index.tsx
@@ -1,13 +1,14 @@
 //Importações
-import * as React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 //Telas
 import { InitialScreen } from "../../pages/Screen-Initial";
 import { ScreenLogin } from '../../pages/Screen-Login';
 import { ScreenSignUp } from '../../pages/Screen-SignUp';
-import {ScreenDetailsEvent} from '../../pages/Screen-Details-Event';
+import { ScreenDetailsEvent } from '../../pages/Screen-Details-Event';
 import { ScreenDetailsVaccine } from '../../pages/Screen-Details-Vaccine';
 import { ScreenEditProfile } from '../../pages/Screen-Edit-Profile';
 import { ScreenVaccinationRequest } from '../../pages/Screen-Vaccination-Requests';
@@ -15,36 +16,50 @@ import { ScreenVaccinationRequest } from '../../pages/Screen-Vaccination-Request
 //Navegação por abas
 import { TabNavigation } from '../TabNavigation';
 
+//Contexto
+import { useAuth } from '../../context/AuthContext';
+
 //Configuração
 const Stack = createNativeStackNavigator();
 
 export const StackNavigation = () => {
+    const { isAuthenticated } = useAuth();
+
     return (
         <NavigationContainer>
             <Stack.Navigator
-                screenOptions={{ 
+                screenOptions={{
                     headerTitleAlign: 'center',
-                    headerTitleStyle: { 
+                    headerTitleStyle: {
                         fontSize: 18,
                     }
                 }}
             >
-                <Stack.Screen 
-                    name="Initial" 
-                    component={InitialScreen} 
-                    options={{ headerShown: false }} //Retirando o cabeçalho
-                />
-                <Stack.Screen name="Login" component={ScreenLogin} options={{ title: "Acessar conta"}}/>
-                <Stack.Screen name="SignUp" component={ScreenSignUp} options={{ title: "Criar conta"}} />
-                <Stack.Screen 
-                    name="TabNaviagtion" 
-                    component={TabNavigation} 
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen name="DetailsEvent" component={ScreenDetailsEvent} options={{ title: "Detalhes do evento"}}/>
-                <Stack.Screen name="DetailsVaccine" component={ScreenDetailsVaccine} options={{ title: "Detalhes da vacina"}}/>
-                <Stack.Screen name="EditProfile" component={ScreenEditProfile} options={{ title: "Edição de Perfil"}}/>
-                <Stack.Screen name="VaccinationRequest" component={ScreenVaccinationRequest} options={{ title: "Solicitações"}}/>
+                {isAuthenticated === false ? ( //Verifica se o usuário está autenticado
+                    //Se não estiver autenticado
+                    <>
+                        <Stack.Screen
+                            name="Initial"
+                            component={InitialScreen}
+                            options={{ headerShown: false }} //Retirando o cabeçalho
+                        />
+                        <Stack.Screen name="Login" component={ScreenLogin} options={{ title: "Acessar conta" }} />
+                        <Stack.Screen name="SignUp" component={ScreenSignUp} options={{ title: "Criar conta" }} />
+                    </>
+                ) : (
+                    //Se estiver autenticado
+                    <>
+                        <Stack.Screen
+                            name="TabNaviagtion"
+                            component={TabNavigation}
+                            options={{ headerShown: false }}
+                        />
+                        <Stack.Screen name="DetailsEvent" component={ScreenDetailsEvent} options={{ title: "Detalhes do evento" }} />
+                        <Stack.Screen name="DetailsVaccine" component={ScreenDetailsVaccine} options={{ title: "Detalhes da vacina" }} />
+                        <Stack.Screen name="EditProfile" component={ScreenEditProfile} options={{ title: "Edição de Perfil" }} />
+                        <Stack.Screen name="VaccinationRequest" component={ScreenVaccinationRequest} options={{ title: "Solicitações" }} />
+                    </>
+                )}
             </Stack.Navigator>
         </NavigationContainer>
     )
