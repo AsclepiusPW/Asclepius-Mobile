90ee9c16af689ddd4f27afedbaec23fbbb1b3089
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-09-01 13:53:29 -0300

    feat: Using User context for get end updating data of user

--- src/pages/Screen-Edit-Profile/index.tsx
+++ src/pages/Screen-Edit-Profile/index.tsx
@@ -1,7 +1,18 @@
 //Importações
-import { useState } from "react";
-import { ScrollView, View, Text, Alert, KeyboardAvoidingView, Platform } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text, Alert, KeyboardAvoidingView, Platform, ActivityIndicator } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
+
+//Context
+import { useUser } from "../../context/UserContext";
+import { useAuth } from "../../context/AuthContext";
+
+//API
+import { Api } from "../../connection/axios";
+import { AxiosError } from "axios";
+
+//Types
+import { ModalType } from "../../../utils/types/typeModal";
 
 //Validação de formulário
 import { useForm, Controller } from 'react-hook-form';
@@ -13,54 +24,93 @@ import { SmallDetailsProfile } from "../../components/Small-Details-Profile";
 import { TouchButton } from "../../components/Touch-Button";
 import { InputForm } from "../../components/Input-Form";
 import { ModalComponent } from "../../components/Modal-Component";
+import { UpdatingComponent } from "../../components/Updating-Component";
 
 //Estilização
 import { ContainerEditProfile, EditProfileHeader, EditProfileForm, ContainerButtonSubmmit } from "./style";
 import { Themes } from "../../../global/theme";
-import React from "react";
+import { User } from "../../../utils/types/typeUser";
 
-//props
-interface props {
-    userName?: string,
-    userEmail?: string,
-    userPhone?: string,
-    userPassword?: string,
-    userImage?: string,
-}
+export const ScreenEditProfile = () => {
+    const { userData, loadDataUser, updateUser } = useUser(); //Buscando dados do usuário
+    const { token } = useAuth(); //Buscando o token do usuário
 
-export const ScreenEditProfile: React.FC<props> = ({ userName, userEmail, userPassword, userPhone, userImage }) => {
     const [profileImage, setProfileImage] = useState<string | null>(null); //State para armazenar a aimagem
     const [modalVisible, setModalVisible] = useState(false);
+    const [loading, setLoading] = useState<boolean>(false); 
+    const [modalType, setModalType] = useState<ModalType>('successfulEdition'); // Estado para o tipo do modal
 
     //Constantes para controle do modal
     const handleOpenModal = () => setModalVisible(true); //Função para abrir o modal
     const handleCloseModal = () => setModalVisible(false); //Função para fechar o modal
 
+    //Pegando informações do usuário do banco de dados
+    useEffect(() => {
+        if (!userData) {
+            setLoading(true);
+            loadDataUser();
+            setLoading(false)
+        }
+    }, [userData]);
+
     //Validação do formulário
-    const { control, handleSubmit, formState: { errors } } = useForm({
+    const { control, handleSubmit, formState: { errors }, setError } = useForm({
         defaultValues: {
-            nameUser: userName || '',
-            emailUser: userEmail || '',
-            phoneUser: userPhone || '',
-            passwordUser: userPassword || '',
+            nameUser: userData?.name || '',
+            emailUser: userData?.email || '',
+            phoneUser: userData?.telefone || '',
+            passwordUser: '', //Senha mantida como vazia
         },
         resolver: yupResolver(validationSchema),
     });
 
-    //Função de enviar formulário (modificar depois essa função)
-    const onSubmit = (data: any) => {
-        Alert.alert(
-            'Formulário enviado com sucesso!',
-            JSON.stringify(data),
-            [
-                {
-                    text: 'OK',
-                    onPress: () => {
-                        handleOpenModal();
-                    }
-                }
-            ]
-        );
+    // Função para carregar a imagem do perfil (depois atualizar)
+    // useEffect(() => {
+    //     if (userData && userData.image) {
+    //         setProfileImage(userData.image);
+    //     }
+    // }, [userData]);
+
+    //Função de enviar formulário
+    const onSubmit = async (data: any) => {
+
+        setLoading(true);
+        try {
+            //Criando objeto de atualização de usuário
+            const updateUserData: User = {
+                name: data.nameUser,
+                email: data.emailUser,
+                telefone: data.phoneUser,
+                latitude: userData?.latitude,
+                longitude: userData?.longitude,
+            };
+
+            //Tratamento da senha
+            if (data.passwordUser) { //Se alterar no formulário
+                updateUserData.password = data.passwordUser
+            }else{ //Caso não alterar
+                updateUserData.password = userData?.password
+            };
+
+            const response = await Api.put("/user/update", updateUserData, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            
+            if (response.status === 201) {
+                await updateUser(updateUserData); //Atualizar o histórico do user
+            }else{
+                setModalType("faliedEdition");
+                Alert.alert('Erro', 'Não foi possível atualizar o perfil. Tente novamente.');
+            }
+        } catch (error) {
+            setModalType("faliedEdition");
+            handleUpdateUserErrors(error);
+        }finally{
+            setLoading(false);
+            handleOpenModal();
+        }
     };
 
     //Função para pegar uma imagem do dispositivo
@@ -81,6 +131,36 @@ export const ScreenEditProfile: React.FC<props> = ({ userName, userEmail, userPa
         }
     };
 
+    //Função para realizar o envio da imagem da foto para a API
+    
+
+    //Função para tratar erros da atualização vindo do backend
+    const handleUpdateUserErrors = (error: unknown) => {
+        if (error instanceof AxiosError) {
+            //Possíveis erros do backend
+            const status = error.response?.status;
+            const backendErrors = error.response?.data;
+
+            if (status === 409 && backendErrors) {
+                let errorMessage = String(backendErrors.error);
+                
+                //Há somente dois erros do backend que a validação não consegue resolver por si só
+                if (errorMessage.includes("e-mail")) {
+                    setError("emailUser", { //Validação de e-mails duplicados
+                        message: "E-mail já existente"
+                    });
+                }else{
+                    setError("phoneUser", { //Validação de telefone duplicados
+                        message: "Telefone já existente"
+                    });
+                }
+            }
+        } else{
+            console.error("Erro inesperado:", (error as Error).message);
+            Alert.alert("Erro", "Ocorreu um erro inesperado. Tente novamente mais tarde.");
+        }
+    }
+
     return (
         <KeyboardAvoidingView
             style={{ flex: 1 }}
@@ -88,87 +168,93 @@ export const ScreenEditProfile: React.FC<props> = ({ userName, userEmail, userPa
             keyboardVerticalOffset={80} // Ajuste o valor conforme necessário
         >
             <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-                <ContainerEditProfile>
+                {
+                    loading ? (
+                        <UpdatingComponent/>
+                    ):(
+                        <ContainerEditProfile>
 
-                    <EditProfileHeader>
-                        <SmallDetailsProfile profileImage={profileImage} />
+                            <EditProfileHeader>
+                                <SmallDetailsProfile profileImage={profileImage} />
 
-                        <TouchButton text="Editar imagem" styleType="buttonSmallSolid" onPress={handleSelectImage} />
-                    </EditProfileHeader>
+                                <TouchButton text="Editar imagem" styleType="buttonSmallSolid" onPress={handleSelectImage} />
+                            </EditProfileHeader>
 
-                    <EditProfileForm>
-                        <Controller
-                            control={control}
-                            name="nameUser"
-                            render={({ field: { onChange, onBlur, value } }) => (
-                                <InputForm
-                                    icon="user"
-                                    placeholder="Nome de Usuário"
-                                    value={value}
-                                    onChangeText={onChange}
-                                    onBlur={onBlur}
+                            <EditProfileForm>
+                                <Controller
+                                    control={control}
+                                    name="nameUser"
+                                    render={({ field: { onChange, onBlur, value } }) => (
+                                        <InputForm
+                                            icon="user"
+                                            placeholder="Nome de Usuário"
+                                            value={value}
+                                            onChangeText={onChange}
+                                            onBlur={onBlur}
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                        {errors.nameUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.nameUser.message}</Text>}
+                                {errors.nameUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.nameUser.message}</Text>}
 
-                        <Controller
-                            control={control}
-                            name="emailUser"
-                            render={({ field: { onChange, onBlur, value } }) => (
-                                <InputForm
-                                    icon="envelope-o"
-                                    placeholder="Email"
-                                    value={value}
-                                    onChangeText={onChange}
-                                    onBlur={onBlur}
-                                    keyboardType="email-address"
+                                <Controller
+                                    control={control}
+                                    name="emailUser"
+                                    render={({ field: { onChange, onBlur, value } }) => (
+                                        <InputForm
+                                            icon="envelope-o"
+                                            placeholder="Email"
+                                            value={value}
+                                            onChangeText={onChange}
+                                            onBlur={onBlur}
+                                            keyboardType="email-address"
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                        {errors.emailUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.emailUser.message}</Text>}
+                                {errors.emailUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.emailUser.message}</Text>}
 
-                        <Controller
-                            control={control}
-                            name="phoneUser"
-                            render={({ field: { onChange, onBlur, value } }) => (
-                                <InputForm
-                                    icon="phone"
-                                    placeholder="Telefone"
-                                    value={value}
-                                    onChangeText={onChange}
-                                    onBlur={onBlur}
-                                    keyboardType="phone-pad"
+                                <Controller
+                                    control={control}
+                                    name="phoneUser"
+                                    render={({ field: { onChange, onBlur, value } }) => (
+                                        <InputForm
+                                            icon="phone"
+                                            placeholder="Telefone"
+                                            value={value}
+                                            onChangeText={onChange}
+                                            onBlur={onBlur}
+                                            keyboardType="phone-pad"
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                        {errors.phoneUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.phoneUser.message}</Text>}
+                                {errors.phoneUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.phoneUser.message}</Text>}
 
-                        <Controller
-                            control={control}
-                            name="passwordUser"
-                            render={({ field: { onChange, onBlur, value } }) => (
-                                <InputForm
-                                    icon="lock"
-                                    placeholder="Senha"
-                                    value={value}
-                                    onChangeText={onChange}
-                                    onBlur={onBlur}
-                                    secureTextEntry={true}
+                                <Controller
+                                    control={control}
+                                    name="passwordUser"
+                                    render={({ field: { onChange, onBlur, value } }) => (
+                                        <InputForm
+                                            icon="lock"
+                                            placeholder="Senha"
+                                            value={value || ""}
+                                            onChangeText={onChange}
+                                            onBlur={onBlur}
+                                            secureTextEntry={true}
+                                        />
+                                    )}
                                 />
-                            )}
-                        />
-                        {errors.passwordUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.passwordUser.message}</Text>}
+                                {errors.passwordUser && <Text style={{ color: `${Themes.colors.redHot}` }}>{errors.passwordUser.message}</Text>}
 
-                        <ContainerButtonSubmmit>
-                            <TouchButton text="Editar" styleType="buttonLargerSolid" onPress={handleSubmit(onSubmit)} />
-                        </ContainerButtonSubmmit>
-                    </EditProfileForm>
+                                <ContainerButtonSubmmit>
+                                    <TouchButton text="Editar" styleType="buttonLargerSolid" onPress={handleSubmit(onSubmit)} />
+                                </ContainerButtonSubmmit>
+                            </EditProfileForm>
 
-                    {/* Modal para opções */}
-                    <ModalComponent visible={modalVisible} onClose={handleCloseModal} typeModal="successfulEdition" />
-                </ContainerEditProfile>
+                            {/* Modal para opções */}
+                            <ModalComponent visible={modalVisible} onClose={handleCloseModal} typeModal={modalType} />
+                        </ContainerEditProfile>
+                    )
+                }
             </ScrollView>
         </KeyboardAvoidingView>
     )
-}
+};
--- src/pages/Screen-Profile/index.tsx
+++ src/pages/Screen-Profile/index.tsx
@@ -1,38 +1,67 @@
 //Importações
-import { ScrollView, View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, Text, ActivityIndicator } from "react-native";
 
 //Componentes
 import { HeaderApresentation } from "../../components/Header-Apresentation"
-
-//Estilização
-import { ContainerProfile } from "./style"
 import { DetailsProfile } from "../../components/Details-Profile";
 import { PresentMap } from "../../components/Present-Map";
 import { VaccinationRequest } from "../../components/Vaccination-Request";
+import { UpdatingComponent } from "../../components/Updating-Component";
+
+//Estilização
+import { ContainerProfile } from "./style"
+
+//Contexto
+import { useUser } from "../../context/UserContext";
 
 export const ScreenProfile = () => {
+    const { userData, loadDataUser } = useUser();
+
+    const [loading, setLoading] = useState<boolean>(false);
+    
+    //Pegando informações do usuário do banco de dados
+    useEffect(() => {
+        if (!userData) {
+            setLoading(true);
+            loadDataUser();
+            setLoading(false)
+        }
+    }, [userData]);
+
     return (
         <ScrollView>
-            <ContainerProfile>
-                <HeaderApresentation
-                    title={"Perfil"}
-                    placeholder="Pesquisar perfil"
-                />
+            {
+                loading ? (
+                    <UpdatingComponent/>
+                ) : (
+                    <ContainerProfile>
 
-                <DetailsProfile
-                    userEmail="userAsclepius@gmail.com"
-                    userName="User Test"
-                    userPhone="(00) 0 0000-0000"
-                />
+                        <HeaderApresentation
+                            title={"Perfil"}
+                        />
 
-                <PresentMap 
-                    zoomEnable={false}
-                    scrollEnable={false}
-                />
+                        <DetailsProfile
+                            userImage={userData && userData.image === "Image not registered" ? "" : userData?.image}
+                            userEmail={userData ? userData.email : "User Not found"}
+                            userName={userData ? userData.name : "User Not found"}
+                            userPhone={userData ? userData.telefone : "User Not found"}
+                        />
 
-                <VaccinationRequest/>
-            </ContainerProfile>
+                        <PresentMap
+                            zoomEnable={false}
+                            scrollEnable={false}
+                            latitude={userData && userData.latitude !== undefined ? userData.latitude.toString() : ""}
+                            longitude={userData && userData.longitude !== undefined ? userData.longitude.toString() : ""}
+                        />
+
+                        { //Atualizar 
+                            userData?.requestReservation && (
+                                <VaccinationRequest />
+                            ) 
+                        }
+                    </ContainerProfile>
+                )}
         </ScrollView>
     )
 }
